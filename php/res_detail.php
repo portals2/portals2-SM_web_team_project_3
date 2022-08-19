961d52cb263c79cff4506bc5460c21300b2b83e0
sun-cheongbi <sunrain625@naver.com>
2022-08-19 14:35:00 +0900

    220819 first file upload css, php page my login and detail

---
+++ php/res_detail.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--뒤로가기 전페이지 ?-->
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
+    <meta name="format-detection" content="telephone=no">
+    <title>미쉐린 가이드 - 상세페이지</title>
+    <link rel="stylesheet" href="res_detail.css">
+    <link rel="stylesheet" href="header.css" />
+</head>
+
+<body>
+    <nav>
+        <div class="logo">
+            <h4>미쉐린 서울 2022</h4>
+        </div>
+        <!-- 메뉴 안에 나오는 글 -->
+        <ul class="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Work</a></li>
+            <li><a href="#">Ya Caramba!</a></li>
+        </ul>
+        <!-- 줄 새개 -->
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+    </nav>
+    <script src="buguer.js"></script>
+    <!--header//-->
+
+    <main class="inner">
+        <div class="wrap">
+            <div class="img_wrap">
+                <img src="/portals2-SM_web_team_project_3/img/res1.jpg">
+            </div>
+            <div class="content">
+                <!--콘텐츠 메인-->
+                <div class="name">
+                    <h3>식당 이름</h3>
+                    <p class="back">백년옥</p>
+                    <img src="/portals2-SM_web_team_project_3/img/level4.png">
+                    <p>빕 구르망; 합리적 가격에 훌륭한 음식</p>
+                </div>
+                <div class="information">
+                    <h3>식당 설명</h3>
+                    <p class="info">예술의전당 맞은편 도로변에 위치한 백년옥은 1992년 영업을 개시한 이래 많은 이들의 사랑을 받는 두부 요리 전문점이다. 세월이 고스란히 묻어나는 소박한 실내는 테이블석과 방석 자리로 나뉘어 있다. 두부 전문점답게 콩비지, 되비지, 신선한 순두부 등 여러 가지 수제 두부 요리를 선보인다. 손님들이 붐비는 식사 시간에 만석일 경우, 백년옥에서 운영하는 오래된 칼국수 전문점</p>
+                </div>
+                <div class="map">
+                    <h3 class="position">위치</h3>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3166.134371770012!2d127.0115801156463!3d37.48115533677028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca1173059a517%3A0x98bac4683c1b3858!2z67Cx64WE7Jil!5e0!3m2!1sko!2skr!4v1660711275287!5m2!1sko!2skr"
+                        style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <p class="tel">식당 번호 : 02-523-2860</p>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer class="footer">
+        <div class="inner">
+            <a href="https://guide.michelin.com/kr/ko">
+                        미쉐린 가이드 2022
+                        </a>
+            <span class="email"> abcdef@naver.com </span>
+        </div>
+    </footer>
+</body>
+
+</html>
